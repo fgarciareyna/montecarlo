f64dccd96c9b71d01d978ac0b8357f6a71a64b26
Facundo García Reyna <facu.garcia@gmail.com>
2017-09-03 01:08:20 -0300

    Estructura inicial

---
+++ Montecarlo/Montecarlo/Probabilidad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Montecarlo
+{
+    public class Probabilidad
+    {
+        public Probabilidad(decimal valor, decimal probabilidadAsociada)
+        {
+            if (probabilidadAsociada < 0 || probabilidadAsociada >1)
+                throw new NotSupportedException("La probabilidad asociada debe ser entre 0 y 1");
+
+            Valor = valor;
+            ProbabilidadAsociada = probabilidadAsociada;
+        }
+
+        public decimal Valor { get; protected set; }
+        public decimal ProbabilidadAsociada { get; protected set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# montecarlo
+# UTN - FRC
+#
+# Simulación
+#
+# TP4: Montecarlo
+#
+#
+# Integrantes:
+#
+# De Franceschi Andrea 62127
+# García Majorel Álvaro 61856
+# García Reyna Facundo 63583
+# Oliveda Claudio 49594
+# Varela Sofía 65126
