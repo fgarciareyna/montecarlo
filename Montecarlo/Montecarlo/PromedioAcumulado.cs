4aac90146b41a5f0abe545267ddc69f7c2cab550
Facundo García Reyna <facu.garcia@gmail.com>
2017-09-03 15:52:01 -0300

    Agregada la gestión de promedios

---
+++ Montecarlo/Montecarlo/PromedioAcumulado.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Montecarlo
+{
+    public class PromedioAcumulado
+    {
+        public PromedioAcumulado()
+        {
+            Cantidad = 0;
+            Promedio = 0;
+        }
+        public PromedioAcumulado(decimal valor)
+        {
+            Cantidad = 1;
+            Promedio = valor;
+        }
+
+        public PromedioAcumulado(int cantidad, decimal promedio)
+        {
+            if (cantidad <= 0)
+                throw new NotSupportedException("La cantidad de valores debe ser un número positivo");
+
+            Cantidad = cantidad;
+            Promedio = promedio;
+        }
+
+        public decimal CalcularSiguiente(decimal valor)
+        {
+            var total = Promedio * Cantidad + valor;
+
+            Cantidad++;
+
+            var promedio = total / Cantidad;
+
+            return promedio;
+        }
+
+        private int Cantidad { get; set; }
+        public decimal Promedio { get; protected set; }
+    }
+}
---
+++ Montecarlo/Montecarlo/PromedioCantidadFija.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Montecarlo
+{
+    public class PromedioCantidadFija
+    {
+        public PromedioCantidadFija(int cantidad)
+        {
+            Cantidad = cantidad;
+            Valores = new List<decimal>(Cantidad);
+        }
+
+        public decimal CalcularSiguiente(decimal valor)
+        {
+            if (Valores.Count == Cantidad)
+                EliminarPrimero();
+
+            Valores.Add(valor);
+
+            var promedio = Valores.Average();
+
+            return promedio;
+        }
+
+        private void EliminarPrimero()
+        {
+            Valores.RemoveAt(0);
+        }
+
+        private int Cantidad { get; }
+        private IList<decimal> Valores { get; }
+    }
+}
