9f5df7883bb84b8c0efebe02394242175d8d6c1b
Facundo García Reyna <facu.garcia@gmail.com>
2017-09-03 21:51:51 -0300

    Más parámetros

--- Montecarlo/TP4/Form1.Designer.cs
+++ Montecarlo/TP4/Form1.Designer.cs
@@ -51,12 +51,20 @@
             this.col_dias_prox_pedido = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.col_dias_demora = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.col_pedido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_mostrar_desde = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_mostrar_cant = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.tb_simulaciones)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // txt_cant_iteraciones
             // 
-            this.txt_cant_iteraciones.Location = new System.Drawing.Point(139, 6);
+            this.txt_cant_iteraciones.Location = new System.Drawing.Point(133, 20);
             this.txt_cant_iteraciones.Name = "txt_cant_iteraciones";
             this.txt_cant_iteraciones.Size = new System.Drawing.Size(36, 20);
             this.txt_cant_iteraciones.TabIndex = 0;
@@ -65,7 +73,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(6, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(121, 13);
             this.label1.TabIndex = 1;
@@ -74,7 +82,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 87);
+            this.label2.Location = new System.Drawing.Point(48, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 3;
@@ -82,7 +90,7 @@
             // 
             // txt_costo_falt
             // 
-            this.txt_costo_falt.Location = new System.Drawing.Point(139, 84);
+            this.txt_costo_falt.Location = new System.Drawing.Point(129, 72);
             this.txt_costo_falt.Name = "txt_costo_falt";
             this.txt_costo_falt.Size = new System.Drawing.Size(36, 20);
             this.txt_costo_falt.TabIndex = 2;
@@ -91,7 +99,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 61);
+            this.label3.Location = new System.Drawing.Point(6, 49);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(117, 13);
             this.label3.TabIndex = 5;
@@ -99,7 +107,7 @@
             // 
             // txt_costo_alm
             // 
-            this.txt_costo_alm.Location = new System.Drawing.Point(139, 58);
+            this.txt_costo_alm.Location = new System.Drawing.Point(129, 46);
             this.txt_costo_alm.Name = "txt_costo_alm";
             this.txt_costo_alm.Size = new System.Drawing.Size(36, 20);
             this.txt_costo_alm.TabIndex = 4;
@@ -108,7 +116,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(66, 35);
+            this.label4.Location = new System.Drawing.Point(56, 23);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(67, 13);
             this.label4.TabIndex = 7;
@@ -116,7 +124,7 @@
             // 
             // txt_stock_inicial
             // 
-            this.txt_stock_inicial.Location = new System.Drawing.Point(139, 32);
+            this.txt_stock_inicial.Location = new System.Drawing.Point(129, 20);
             this.txt_stock_inicial.Name = "txt_stock_inicial";
             this.txt_stock_inicial.Size = new System.Drawing.Size(36, 20);
             this.txt_stock_inicial.TabIndex = 6;
@@ -124,7 +132,7 @@
             // 
             // btn_simular
             // 
-            this.btn_simular.Location = new System.Drawing.Point(100, 110);
+            this.btn_simular.Location = new System.Drawing.Point(434, 82);
             this.btn_simular.Name = "btn_simular";
             this.btn_simular.Size = new System.Drawing.Size(75, 23);
             this.btn_simular.TabIndex = 8;
@@ -238,27 +246,88 @@
             this.col_pedido.Name = "col_pedido";
             this.col_pedido.ReadOnly = true;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txt_stock_inicial);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_costo_alm);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_costo_falt);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 107);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parámetros iniciales";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txt_mostrar_desde);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.txt_mostrar_cant);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txt_cant_iteraciones);
+            this.groupBox2.Location = new System.Drawing.Point(218, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(192, 107);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Iteraciones";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Mostrar desde:";
+            // 
+            // txt_mostrar_desde
+            // 
+            this.txt_mostrar_desde.Location = new System.Drawing.Point(133, 46);
+            this.txt_mostrar_desde.Name = "txt_mostrar_desde";
+            this.txt_mostrar_desde.Size = new System.Drawing.Size(36, 20);
+            this.txt_mostrar_desde.TabIndex = 4;
+            this.txt_mostrar_desde.Text = "1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Cantidad a mostrar:";
+            // 
+            // txt_mostrar_cant
+            // 
+            this.txt_mostrar_cant.Location = new System.Drawing.Point(133, 72);
+            this.txt_mostrar_cant.Name = "txt_mostrar_cant";
+            this.txt_mostrar_cant.Size = new System.Drawing.Size(36, 20);
+            this.txt_mostrar_cant.TabIndex = 2;
+            this.txt_mostrar_cant.Text = "10";
+            // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1046, 483);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.tb_simulaciones);
             this.Controls.Add(this.btn_simular);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.txt_stock_inicial);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txt_costo_alm);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txt_costo_falt);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txt_cant_iteraciones);
             this.Name = "Principal";
             this.Text = "De Franceschi Andrea 62127 - García Majorel Álvaro 61856 - García Reyna Facundo 6" +
     "3583 - Oliveda Claudio 49594 - Varela Sofía 65126";
             ((System.ComponentModel.ISupportInitialize)(this.tb_simulaciones)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -287,6 +356,12 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn col_dias_prox_pedido;
         private System.Windows.Forms.DataGridViewTextBoxColumn col_dias_demora;
         private System.Windows.Forms.DataGridViewTextBoxColumn col_pedido;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_mostrar_desde;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_mostrar_cant;
     }
 }
 
